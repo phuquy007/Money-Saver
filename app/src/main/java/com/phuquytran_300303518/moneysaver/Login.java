c0ee627332e4878927617633becccf853c82720b
Ryan Tran <tranp6@student.douglascollege.ca>
2021-10-08 21:32:05 -0700

    initialize project

---
+++ app/src/main/java/com/phuquytran_300303518/moneysaver/Login.java
@@ -0,0 +1,14 @@
+package com.phuquytran_300303518.moneysaver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
