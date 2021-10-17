dd786f2c7729d68225cc661b0c63d7e80277bb75
An Doan <phuandoan1012@gmail.com>
2021-10-17 09:53:49 +0700

    Add user interface for Plan and Report fragment
    - Fixed the hint color for Login + Register feature. Correct Username -> Email label.
    
    Not add data to Recycler View in Plan fragment yet.
    Need to fix the legend and chart style in Report Fragment.

--- app/src/main/java/com/phuquytran_300303518/moneysaver/Fragments/ReportFragment.java
+++ app/src/main/java/com/phuquytran_300303518/moneysaver/Fragments/ReportFragment.java
@@ -1,15 +1,30 @@
 package com.phuquytran_300303518.moneysaver.Fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.phuquytran_300303518.moneysaver.R;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import java.util.ArrayList;
+
 public class ReportFragment extends Fragment {
+
+    PieChart pieChart;
+    PieData pieData;
+    PieDataSet pieDataSet;
+    ArrayList pieEntries;
+    ArrayList PieEntryLabels;
 
     public ReportFragment() {
 
@@ -18,12 +33,38 @@ public class ReportFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //Inflate the transaction fragment view
+        View view = inflater.inflate(R.layout.fragment_report, container, false);
+
+        pieChart = view.findViewById(R.id.pieChart);
+        getEntries();
+        pieDataSet = new PieDataSet(pieEntries, "");
+        pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieDataSet.setColors(ColorTemplate.LIBERTY_COLORS);
+        pieDataSet.setSliceSpace(2f);
+        pieDataSet.setValueTextColor(Color.WHITE);
+        pieDataSet.setValueTextSize(10f);
+        pieDataSet.setSliceSpace(5f);
+        pieChart.setEntryLabelColor(Color.WHITE);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false);
+        return view;
+    }
+
+    private void getEntries() {
+        pieEntries = new ArrayList<>();
+        pieEntries.add(new PieEntry(2f, "Food"));
+        pieEntries.add(new PieEntry(4f, "Transport"));
+        pieEntries.add(new PieEntry(6f, "Rent"));
+        pieEntries.add(new PieEntry(8f, "Clothes"));
+        pieEntries.add(new PieEntry(7f, "Travel"));
+        pieEntries.add(new PieEntry(3f, "Saving"));
     }
 }
