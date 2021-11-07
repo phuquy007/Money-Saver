2d17270bd849e8192faf4174ec6c2f2549bbf9b6
An Doan <phuandoan1012@gmail.com>
2021-11-07 22:15:48 +0700

    fix Achievement Fragment
    - Apply all transaction data from the database to calculate the total saving of each user.
    - Fix spelling error: Archievement -> Achievement

--- app/src/main/java/com/phuquytran_300303518/moneysaver/Activities/MainActivity.java
+++ app/src/main/java/com/phuquytran_300303518/moneysaver/Activities/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     TransactionFragment transactionFragment;
     PlanFragment planFragment;
     ReportFragment reportFragment;
-    AchievementFragment archievementFragment;
+    AchievementFragment achievementFragment;
     FragmentManager fragmentManager;
     List<Transaction> transactions;
     FirebaseDatabase database;
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         transactionFragment = new TransactionFragment(fragmentManager);
         planFragment = new PlanFragment();
         reportFragment = new ReportFragment();
-        archievementFragment = new AchievementFragment();
+        achievementFragment = new AchievementFragment();
 
 //        toolbar = findViewById(R.id.toolbar);
 
@@ -82,8 +82,8 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.report:
                     fragmentManager.beginTransaction().replace(R.id.nav_fragment, reportFragment).commit();
                     return true;
-                case R.id.archievement:
-                    fragmentManager.beginTransaction().replace(R.id.nav_fragment, archievementFragment).commit();
+                case R.id.achievement:
+                    fragmentManager.beginTransaction().replace(R.id.nav_fragment, achievementFragment).commit();
                     return true;
             }
             return false;
--- app/src/main/java/com/phuquytran_300303518/moneysaver/Fragments/AchievementFragment.java
+++ app/src/main/java/com/phuquytran_300303518/moneysaver/Fragments/AchievementFragment.java
@@ -6,13 +6,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.phuquytran_300303518.moneysaver.Entities.Transaction;
 import com.phuquytran_300303518.moneysaver.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AchievementFragment extends Fragment {
     TextView txtSaving, txtShoes, txtCar, txtHouse, txtPhone;
-    int saving = 50000;
+    double saving = 50000;
+
+    List<Transaction> transactions;
+    FirebaseDatabase database;
+    DatabaseReference databaseReference;
+
 
     public AchievementFragment() {
         // Required empty public constructor
@@ -28,38 +45,62 @@ public class AchievementFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_achievement, container, false);
 
-        txtSaving = view.findViewById(R.id.txtSaving);
-        txtSaving.setText(saving + "$");
+        transactions = new ArrayList<>();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference(user.getUid());
 
-        txtShoes = view.findViewById(R.id.txtShoes);
-        if (saving >= 200){
-            txtShoes.setText("New shoes unlocked");
-        } else {
-            txtShoes.setText("Unlocking " + ((int)(saving/200.0*100)) + "% new shoes");
-        }
+        DatabaseReference transactionRef = databaseReference.child("transactions");
+        transactionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                transactions.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Transaction transaction = dataSnapshot.getValue(Transaction.class);
+                    transactions.add(transaction);
+                }
 
-        txtPhone = view.findViewById(R.id.txtPhone);
-        if (saving >= 600){
-            txtPhone.setText("New phone unlocked");
-        } else {
-            txtPhone.setText("Unlocking " + ((int)(saving/600.0*100))+ "% new phone");
-        }
+                saving = Double.parseDouble(TransactionFragment.getInFlow(transactions)) - Double.parseDouble(TransactionFragment.getOutFlow(transactions));
+                txtSaving = view.findViewById(R.id.txtSaving);
+                txtSaving.setText(String.format("%.2f",saving) + "$");
 
-        txtCar = view.findViewById(R.id.txtCar);
-        if (saving >= 50000){
-            txtCar.setText("New car unlocked");
-        } else {
-            txtCar.setText("Unlocking " + ((int)(saving/50000.0*100)) + "% new car");
-        }
+                txtShoes = view.findViewById(R.id.txtShoes);
+                if (saving >= 200) {
+                    txtShoes.setText("New shoes unlocked");
+                } else {
+                    txtShoes.setText("Unlocking " + ((int) (saving / 200.0 * 100)) + "% new shoes");
+                }
 
-        txtHouse = view.findViewById(R.id.txtHouse);
-        if (saving >= 1000000){
-            txtHouse.setText("New house unlocked");
-        } else {
-            txtHouse.setText("Unlocking " + ((int) (saving/1000000.0*100)) + "% new house");
-        }
+                txtPhone = view.findViewById(R.id.txtPhone);
+                if (saving >= 600) {
+                    txtPhone.setText("New phone unlocked");
+                } else {
+                    txtPhone.setText("Unlocking " + ((int) (saving / 600.0 * 100)) + "% new phone");
+                }
+
+                txtCar = view.findViewById(R.id.txtCar);
+                if (saving >= 50000) {
+                    txtCar.setText("New car unlocked");
+                } else {
+                    txtCar.setText("Unlocking " + ((int) (saving / 50000.0 * 100)) + "% new car");
+                }
+
+                txtHouse = view.findViewById(R.id.txtHouse);
+                if (saving >= 1000000) {
+                    txtHouse.setText("New house unlocked");
+                } else {
+                    txtHouse.setText("Unlocking " + ((int) (saving / 1000000.0 * 100)) + "% new house");
+                }
+            }
+                @Override
+                public void onCancelled (@NonNull DatabaseError error){
+
+                }
+            });
 
         // Inflate the layout for this fragment
         return view;
     }
-}
+
+
+        }
